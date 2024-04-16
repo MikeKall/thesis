7d2e1a87942ce3f999c63924db2ed7332ac159b0
Mike Kalliafas <kalliafasmixalis@proton.me>
2024-04-16 16:59:23 +0300

    Get vulnerabilities from local file based on the service version

--- lib/CVEFetcher.py
+++ lib/CVEFetcher.py
@@ -6,6 +6,8 @@ import json
 import os
 from os.path import exists
 import re
+from packaging.version import Version
+from pprint import pprint
 
 class CVEFetcher():
     def __init__(self, versions):
@@ -15,17 +17,18 @@ class CVEFetcher():
         cached = False
         cached_cves_f = [pos_json for pos_json in os.listdir('.') if pos_json.startswith('CachedCVEs')]
         if cached_cves_f:
-            if exists(cached_cves_f):
+            if exists(cached_cves_f[0]):
                 if not os.stat(cached_cves_f[0]).st_size == 0:
                     print("Cached file found")
                     cached = True
                     vulnerabilities = self.get_CVEs_Local(cached_cves_f[0])
 
                     return vulnerabilities, cached
+
         return self.get_CVEs_NIST(), cached
 
     def get_CVEs_Local(self, cached_cves_f):
-        pattern = r"_(.*?).json"
+        pattern = r"(\d.*?).json"
         match = re.search(pattern, cached_cves_f)
         date_str = match.group(1)
         current_dtime = datetime.today().strftime('%Y_%m_%d')
@@ -33,7 +36,6 @@ class CVEFetcher():
         current_dtime = datetime.strptime(current_dtime, "%Y_%m_%d")
         
         delta = current_dtime - file_date
-        print(f"Delta is {delta}")
         if delta.days < 7:
             with open(cached_cves_f, "r") as f:
                 loaded_json = json.load(f)    
@@ -43,28 +45,83 @@ class CVEFetcher():
         vulnerabilities = {}
         auth = HTTPBasicAuth("apiKey", "9a9374cd-04e7-4706-ae4c-fa4855a8f846")
         headers = {"Accept": "application/json"}
-        dtime = datetime.today().strftime('%Y_%m_%d')
+        
+        for service in self.versions:
+            resultsPerPage = 0
+            if not self.versions[service] == "Unknown":
+                print(f"Searching CVEs for service {service}")
+                url = f"https://services.nvd.nist.gov/rest/json/cves/2.0?keywordSearch={service}"
+                response = requests.get(url, headers=headers, auth=auth)
 
-
-        with open(f"CachedCVEs{dtime}.json", "w") as f:
-            for service in self.versions:
-                resultsPerPage = 0
-                if not self.versions[service] == "Unknown":
-                    print(f"Searching CVEs for service {service}")
-                    url = f"https://services.nvd.nist.gov/rest/json/cves/2.0?keywordSearch={service}"
-                    response = requests.get(url, headers=headers, auth=auth)
-                    while not response.status_code == 200:
-                        time.sleep(6)
-                        response = requests.get(url, headers=headers, auth=auth)
-                    data = json.loads(response.text)
-                    while data["totalResults"] > data["resultsPerPage"]:
-                        resultsPerPage += data["resultsPerPage"]
-                        url = f"https://services.nvd.nist.gov/rest/json/cves/2.0?startIndex={resultsPerPage}&keywordSearch={service}"
-                        response = requests.get(url, headers=headers, auth=auth)
-                        data = json.loads(response.text)
-
-                        json.dump(data, f)
-
-                    vulnerabilities[service] = data
+                while not response.status_code == 200:
                     time.sleep(6)
+                    response = requests.get(url, headers=headers, auth=auth)
+                data = json.loads(response.text)
+                while data["totalResults"] > data["resultsPerPage"]:
+                    resultsPerPage += data["resultsPerPage"]
+                    url = f"https://services.nvd.nist.gov/rest/json/cves/2.0?startIndex={resultsPerPage}&keywordSearch={service}"
+                    response = requests.get(url, headers=headers, auth=auth)
+                    data = json.loads(response.text)
+                vulnerabilities[service] = data
+                time.sleep(6)
         return vulnerabilities
+    
+    
+    def writeTofile(self, data):
+        dtime = datetime.today().strftime('%Y_%m_%d')
+        with open(f"CachedCVEs{dtime}.json", "w+") as f:
+            json.dump(data, f)
+
+    def CVEfilter(self, versions, vulnerabilities):
+        active_vulnerbilities = {}
+        possible_vulnerabilities = {}
+        for service_name, service_version in versions.items():
+            if not service_version == "Unknown":
+                index = 0
+                has_startingVersion = True
+                has_endingVersion = True
+                while vulnerabilities[service_name]['resultsPerPage'] > 0:
+                    try:
+                        starting_version = vulnerabilities[service_name]['vulnerabilities'][index]['cve']['configurations'][0]["nodes"][0]["cpeMatch"][0]["versionEndIncluding"]
+                    except:
+                        has_startingVersion = False
+                        pass
+                    
+                    try:
+                        ending_version = vulnerabilities[service_name]['vulnerabilities'][index]['cve']['configurations'][0]["nodes"][0]["cpeMatch"][0]["versionEndIncluding"]
+                    except:
+                        has_endingVersion = False
+                        pass
+                    
+                    pattern = r"^([\d.-]+)"
+                    match = re.search(pattern, service_version)
+                    
+                    if match:
+                        print("IN")
+                        service_version = match.group(0)
+                        if has_startingVersion and has_endingVersion:
+                            if Version(starting_version) <= Version(service_version) <= Version(ending_version):
+                                active_vulnerbilities[service_name] = {"CVE": vulnerabilities[service_name]['vulnerabilities'][index]['cve']['id'],
+                                                                        "Severity": vulnerabilities[service_name]['vulnerabilities'][index]['cve']['metrics']['cvssMetricV2'][0]['baseSeverity'],
+                                                                        "Exploitability Score": vulnerabilities[service_name]['vulnerabilities'][index]['cve']['metrics']['cvssMetricV2'][0]['exploitabilityScore'],
+                                                                        "Impact Score": vulnerabilities[service_name]['vulnerabilities'][index]['cve']['metrics']['cvssMetricV2'][0]['impactScore']
+                                                                        }
+                                break
+                        elif has_endingVersion:
+                            if Version(service_version) <= Version(ending_version):
+                                active_vulnerbilities[service_name] = {"CVE": vulnerabilities[service_name]['vulnerabilities'][index]['cve']['id'],
+                                                                        "Severity": vulnerabilities[service_name]['vulnerabilities'][index]['cve']['metrics']['cvssMetricV2'][0]['baseSeverity'],
+                                                                        "Exploitability Score": vulnerabilities[service_name]['vulnerabilities'][index]['cve']['metrics']['cvssMetricV2'][0]['exploitabilityScore'],
+                                                                        "Impact Score": vulnerabilities[service_name]['vulnerabilities'][index]['cve']['metrics']['cvssMetricV2'][0]['impactScore']
+                                                                        }
+                            break
+                        else:
+                            possible_vulnerabilities[service_name] = {"CVE": vulnerabilities[service_name]['vulnerabilities'][index]['cve']['id'],
+                                                                        "Severity": vulnerabilities[service_name]['vulnerabilities'][index]['cve']['metrics']['cvssMetricV2'][0]['baseSeverity'],
+                                                                        "Exploitability Score": vulnerabilities[service_name]['vulnerabilities'][index]['cve']['metrics']['cvssMetricV2'][0]['exploitabilityScore'],
+                                                                        "Impact Score": vulnerabilities[service_name]['vulnerabilities'][index]['cve']['metrics']['cvssMetricV2'][0]['impactScore']
+                                                                        }
+                            break
+                    print(index)
+                    index += 1
+        return active_vulnerbilities, possible_vulnerabilities
