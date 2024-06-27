0d85ce2b9b20fdc16a8370489969a36eb0448139
MikeKall <kalliafasmixalis@proton.me>
2024-06-27 12:26:32 +0300

    Imporved design for the User assessment scripts

--- lib/Users/WinUserAssessment.py
+++ lib/Users/WinUserAssessment.py
@@ -1,34 +1,10 @@
 import subprocess
 from progressbar import ProgressBar, Percentage, Bar, RotatingMarker, ETA, Timer, AdaptiveETA
 import time
-import re
 
-class assess_users():
+class WinUserAssessment():
     
-    def __init__(self, distro, os):
-        super(assess_users, self).__init__()
-        self.distro = distro
-        self.os = os
-
     def GetUsers(self):
-        if self.os == "windows":
-            return self.GetWinUsers()
-        
-        elif self.os == "linux":
-            pattern = r"^(.*?):(.*?):"
-            local_users = []
-            users = subprocess.run(["cat", "/etc/shadow"], stdout=subprocess.PIPE).stdout.decode().split("\n")
-            for user in users:
-                captures = re.search(pattern, user)
-                if captures:
-                    if captures.groups()[0] and len(captures.groups()[1])>=3:
-                        local_users.append(captures.groups()[0])
-            
-            return local_users
-        else:
-            print(f"Couldn't find os {self.os}")
-
-    def GetWinUsers(self):
         cmd = ['powershell', '-c', 'Get-WmiObject -Class Win32_UserAccount | foreach { $_.Caption }']
         proc = (subprocess.run(cmd, capture_output=True)).stdout.decode().split("\n")
         local_users = []
@@ -37,19 +13,8 @@ class assess_users():
         
         return local_users
     
-    def ReadWordlist(self, wordlist_f):
-        wordlist = []
-        print("Loading wordlist. This might take a while.")
-        with open(wordlist_f, 'r', encoding="utf8") as file:
-            content = file.read().splitlines()
-        
-        for line in content:
-            wordlist.append(line)
-
-        print("Wordlist loaded\n")
-        return wordlist
     
-    def WinPassCracker(self, wordlist, local_user):
+    def PassCracker(self, wordlist, local_user):
         length = len(wordlist)
         count = 0
         widgets = ['Progress: ', Percentage(), ' | ', Timer(), ' | ', AdaptiveETA()]
@@ -76,38 +41,6 @@ class assess_users():
 
         return False, password
     
-
-    def LinuxPassCracker(self, wordlist, local_user):
-        #
-        # yescript nightmare
-        #
-
-        length = len(wordlist)
-        count = 0
-        widgets = ['Progress: ', Percentage(), ' | ', Timer(), ' | ', AdaptiveETA()]
-        bar = ProgressBar(widgets=widgets, max_value=100).start()
-        for password in wordlist:
-            cmd = [f"su", "-l", local_user]
-            
-            proc = subprocess.run(cmd, input=password.encode(), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-            #time.sleep(1) # Depends on how fast the PC is. If it's slow, without a built in delay, there will be false positives
-            if proc.returncode == 0:
-                bar.update(100)
-                return True, password
-            
-            count+=1
-            bar.update(self.TranslateTo100(count, length))
-
-        return False, password
-
-
-    def PassCracker(self, wordlist, local_user):
-        if self.os == "windows":
-            return self.WinPassCracker(wordlist, local_user)
-        elif self.os == "linux":
-            return self.LinuxPassCracker(wordlist, local_user)
-
-
     def PrivilagedGroupsMember(self, vulnerable_user):
         cmd1 = ['powershell', '-c', 'Get-LocalGroupMember -name "Administrators" | foreach {$_.Name}']
         cmd2 = ['powershell', '-c', 'Get-LocalGroupMember -name "Backup Operators" | foreach {$_.Name}']
